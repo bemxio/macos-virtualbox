652814bde7d17ccff3aa3da24f5b72f1c82ea6cb
bemxio <bemxiov@protonmail.com>
2025-04-14 16:49:32 +0200

    remove -ps flag from xxd -e flag check

--- macos-guest-virtualbox.sh
+++ macos-guest-virtualbox.sh
@@ -246,7 +246,7 @@ if [[ -z "$(gzip --help 2>/dev/null)" ||
 fi
 
 # check that xxd supports endianness -e flag
-if [[ -z "$(xxd -e -ps -l 16 /dev/urandom 2>/dev/null)" ]]; then
+if [[ -z "$(xxd -e -l 16 /dev/urandom 2>/dev/null)" ]]; then
     echo "Please make sure a version of xxd which supports the -e option is installed."
     echo -e "The -e option should be listed when executing   ${low_contrast_color}xxd --help${default_color}"
     echo "The package vim-common-8 provides a compatible version on most modern distros."
